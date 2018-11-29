75de0622d42fd1704aaef65f995752ea895bb7f6
Luis Louro <lapisdecor@gmail.com>
2018-11-29 20:30:54 +0000

    Add functions to count days and convert string to datetime object

---
+++ dontsmoke.py
@@ -0,0 +1,18 @@
+"""
+Calculate the time since you stoped smoking.
+"""
+
+from datetime import datetime
+
+def how_many_days(stoping_date):
+    current_date = datetime.now()
+    return current_date - stoping_date
+
+def parse_date(stoping_date):
+    datetime_object = datetime.strptime(stoping_date, '%d-%m-%Y')
+    return datetime_object
+
+stoping_date = input("When did you stoped smoking (dd-mm-yyyy)? ")
+
+print(how_many_days(parse_date(stoping_date)))
+
